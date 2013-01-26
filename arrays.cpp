4ce95b257a42824dddcbdf6d4fa1ae368b52ecf1
Harsh <harsh@harsh-ThinkPad-X220.(none)>
2013-01-26 03:09:11 -0500

    Need testing

--- arrays.cpp
+++ arrays.cpp
@@ -2,11 +2,11 @@
 #include<vector>
 #include<bitset>
 #include<map>
-
+#include<algorithm>
 using namespace std;
 
 
-//Exponentiation via repeated squaring
+//Exponentiation via repeated squaring trick O(N^3)
 long int exponentiation(int A, int i, int R){
   int c = 0;
   int d = 1;
@@ -30,16 +30,37 @@ long int exponentiation(int A, int i, int R){
 //   cout<<endl;
 // }
 
-void compute( int A, int B, int C, int R, int K, int N){
+long int compute( int A, int B, int C, int R, vector< long int>&M, int K, int N){
   
   for(int i = 1; i < K; i++ ){
     M.push_back((exponentiation(A,i+1,R)*B + C)%R);
   }
   
   //Finding the minimum integer not contained in the previous K elements
-  //Is there a formula here? 
-  //Missing values
+  //Is there a formula here, for the following? 
+    
+  vector<pair<long int,long int> > P; 
+  int result,max = 0;
 
+  for(int i = 0; i < M.size(); i++){
+    P.push_back(make_pair(M[i],i));
+  }
+  sort(P.begin(),P.end());
+  max = P[P.size()-1].first; 
+
+  //Retrieve the Missing values upto K+size(new_values) <= N
+  for(int i = 0; i < P.size() - 1; i++){
+    for(int j = P[i].first+1; j < P[i+1].first && M.size() <= N;  j++){
+      M.push_back(j); //Append new_values
+    }
+    if(M.size() == N)
+      result = M[M.size() - 1];
+  }
+  
+  //We have an array of size K+size(new_values) < N 
+  result = max + N - M.size();
+  
+  return result; 
 }
 
 int main(){
@@ -54,11 +75,9 @@ int main(){
     cin>>A>>B>>C>>R;
     M.push_back(A); 
     if(N < K) result = exponentiation(A,N,R);
-    else  result = compute(A,B,C,R,K,N);
+    else  result = compute(A,B,C,R,M,K,N);
     case_num++;
     cout<<"Case #"<<case_num<<": "<<result<<endl;
-    
-    
   }
 
 }
